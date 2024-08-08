d625d7af7d64bb533330d6fefaf8cbf485809550
PatelShakilICT <patelshakil10@gmail.com>
2024-08-08 17:07:22 +0530

    fthfgh

--- api/signup.php
+++ api/signup.php
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->store_result();
 
     if ($stmt->num_rows > 0) {
-        $response['status'] = 'error';
+        $response['status'] = false;
         $response['message'] = 'Email already registered';
     } else {
         // Hash the password
