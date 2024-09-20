f305249e3b43420085f322c0dc9c20c06b282beb
PatelShakil <patelsakib95@gmail.com>
2024-09-20 21:37:14 +0530

    asdf

---
+++ api/get-forms.php
@@ -0,0 +1,72 @@
+<?php
+// Allow from any origin
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: http://localhost:5173");  // Specify your React dev server's URL
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+require 'connection.php';
+
+
+function getForm($tbl,$conn){
+    $sql = "SELECT * FROM $tbl";
+    $stmt = $conn->prepare($sql);
+
+    if ($stmt) {
+        $stmt->execute();
+        $result = $stmt->get_result();  // Get the result set from the prepared statement
+
+        if ($result->num_rows > 0) {
+            $data = $result->fetch_all(MYSQLI_ASSOC);  // Fetch all users as an associative array
+
+            $response['status'] = true;
+            $response['data'] = $data;
+            $response['message'] = 'Users found';
+        } else {
+            $response['status'] = false;
+            $response['message'] = 'No users found';
+        }
+
+        $stmt->close();
+    } else {
+        $response['status'] = false;
+        $response['message'] = 'Failed to prepare the statement';
+    }
+    return $response;
+}
+
+// Initialize response array
+$response = array();
+$response['data'] = null;
+$response['message'] = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) {
+
+    $id = $_GET["id"];
+
+    switch($id){
+        case 1 :{
+            $response = getForm("form_a",$conn);
+        
+            break;
+        }
+        case 2 :{
+            
+            break;
+        }
+
+    }
+
+
+    
+}else{
+    $response['status'] = false;
+    $response['message'] = 'Invalid request method';
+}
+
+// Close connection
+$conn->close();
+
+// Output the response in JSON format
+echo json_encode($response);
+
