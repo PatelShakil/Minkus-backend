5ac3de2e7e93a809f9ed1d75e95b58196a42e162
PatelShakil <patelsakib95@gmail.com>
2024-10-12 13:34:17 +0530

    sdf

--- api/get_protocols.php
+++ api/get_protocols.php
@@ -2,7 +2,8 @@
 // Allow from any origin
 header('Content-Type: application/json');
 $allowed_origins = ['http://localhost:5173', 'https://admin.app-minkus.com'];
-if (in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
     header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
 }
 
@@ -10,11 +11,6 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
 require 'connection.php';
-
-// Initialize response array
-$response = array();
-$response['data'] = null;
-$response['message'] = "";
 
 // Fetch all protocol subscriptions
 $sql = "SELECT * FROM protocol_subs";
